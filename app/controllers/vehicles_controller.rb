f2bae6b96ac53f42ada7dbff792d25577bcc6e7e
Leslie-Gu <snowberg.prc@gmail.com>
2022-05-12 22:11:43 +1000

    view setting up

---
+++ app/controllers/vehicles_controller.rb
@@ -0,0 +1,55 @@
+class VehiclesController < ApplicationController
+
+  before_action :check_auth
+  before_action :set_vehicle, only: [:show, :edit, :update, :destroy, :booking, :cancel_booking]
+
+  def index
+    @vehicles = Vehicle.all
+  end
+
+  def show
+  end
+
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def booking
+  end
+
+  def cancel_booking
+  end
+
+  def create
+    @vehicle = Vehicle.create!(vehicle_params)
+    redirect_to @vehicle
+  end
+
+  
+
+  def edit
+  end
+
+  def update
+    @vehicle = Vehicle.update!(vehicle_params)
+    redirect_to @vehicle
+  end
+
+  def destroy
+    @vehicle.destroy
+    redirect_to vehicles_path
+  end
+
+  private
+  def check_auth
+    authorize Vehicle
+  end
+
+  def set_vehicle
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def vehicle_params 
+    return params.require(:vehicle).permit().merge(user: current_user)
+  end
+end
